3fce5743dfdedbf6a070602fb370a2e11a9f0ab6
GARUM Fundatio <juantomas@lared.es>
2010-12-08 00:50:33 +0100

    codigo base ubicaciones

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do |map|
 
-  # montar_en = Bazarcms::Engine.config.montar_en
-
-  # match "/bazarcms" => 'bazarcms/empresas#index'
-
   match "/bazarcms/datos" => "bazarcms/empresasdatos#index"
   match "/bazarcms/dashboard" => "bazarcms/empresas#dashboard"
   
+  map.resources :ubicaciones, 
+                          :controller => "bazarcms/ubicaciones",
+                          :path_prefix => "/bazarcms/",
+                          :name_prefix => "bazarcms_"
+ 
   map.resources :empresas, #:only => [ :index, :show, :edit, :update ],
                           :controller => "bazarcms/empresas",
                           :path_prefix => "/bazarcms/",
