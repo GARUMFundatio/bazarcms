61aefb78e82f5e07ef4ea8c2d3be08a74c215969
GARUM Fundatio <juantomas@lared.es>
2011-01-12 13:25:52 +0100

    implementando los filtros para empleados en las busquedas

--- app/controllers/bazarcms/empresas_controller.rb
+++ app/controllers/bazarcms/empresas_controller.rb
@@ -163,7 +163,7 @@ module Bazarcms
      
       if micluster != cluster.id 
         
-        uri = URI.parse("#{cluster.url}/bazarcms/buscaempresas?q="+CGI.escape(params[:q])+"&bid=#{@consulta.id}&cid=#{micluster}")
+        uri = URI.parse("#{cluster.url}/bazarcms/buscaempresas?q="+CGI.escape(params[:q])+"qe="+CGI.escape(params[:qe])+"&bid=#{@consulta.id}&cid=#{micluster}")
 
         post_body = []
         post_body << "Content-Type: text/plain\r\n"
