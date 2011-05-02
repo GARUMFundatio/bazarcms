b3d57dd15761c10b8195b0ca4d38f0363eaeb38c
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-05-02 12:09:48 +0200

    Busqueda de ofertas: reorganizando

--- config/routes.rb
+++ config/routes.rb
@@ -84,6 +84,7 @@ Bazar::Application.routes.draw do
 
   match "/bazarcms/publicaroferta" => 'bazarcms/ofertas#new', :as => :publicaroferta 
   match "/bazarcms/enviaroferta" => 'bazarcms/ofertas#enviaroferta', :as => :enviaroferta 
+  match "/bazarcms/buscaofertas" => "bazarcms/ofertas#busca"
   match "/ofertas.rss" => 'bazarcms/ofertas#rss'
 
   match "/bazarcms/hydra" => 'bazarcms/empresas#hydra', :as => :hydra
