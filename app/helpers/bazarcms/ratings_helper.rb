155c1bc7b7593ef625081554f3b9d3e5c0e6a1d9
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-06-28 12:53:29 +0200

    Rating: preparando el formulario de entrada

---
+++ app/helpers/bazarcms/ratings_helper.rb
@@ -0,0 +1,48 @@
+module Bazarcms
+  module RatingsHelper
+  
+    def helper_for_widgets_view
+      "esta salida viene del BazarCMSHelper"
+    end
+
+    def helper_rating_show(valor)
+      
+      val = "#{valor}".split('.')[0]
+      str = "<div>" 
+      
+      for ii in ['1', '2', '3', '4', '5'] 
+      
+        if (ii > val) 
+          str += "<img src='/images/addfav.png'>"
+        else 
+          str += "<img src='/images/rating.png'>"
+        end 
+        
+      end 
+      
+      str += "</div>"
+      str 
+    end
+
+    def helper_rating_calc(valor)
+      
+      val = "#{valor}".split('.')[0]
+      str = "<div>" 
+      
+      for ii in ['1', '2', '3', '4', '5'] 
+      
+        if (ii > val) 
+          str += "<img src='/images/addfav.png'>"
+        else 
+          str += "<img src='/images/rating.png'>"
+        end 
+        
+      end 
+      
+      str += "</div>"
+      str 
+    end
+
+    
+  end
+end
