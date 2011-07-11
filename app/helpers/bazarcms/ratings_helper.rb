8f0587a832fc738feec466b6ed8d50da146dee34
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-07-11 23:02:38 +0200

    Rating: calculo rating

--- app/helpers/bazarcms/ratings_helper.rb
+++ app/helpers/bazarcms/ratings_helper.rb
@@ -24,6 +24,43 @@ module Bazarcms
       str 
     end
 
+    def helper_rating_show2(bazar, empresa)
+      
+      if (bazar == BZ_param("BazarId"))
+        
+        empresa = Bazarcms::Empresa.find_by_id(empresa)
+        
+        valor = empresa.rating
+        nombre = empresa.nombre
+        
+        url = "/bazarcms/ratings/new?bazar_id=#{bazar}&empresa_id=#{empresa}&empresa_nombre=#{nombre}"
+        
+      else 
+        
+        valor = 0
+        url = ""
+      end
+      
+      val = "#{valor}".split('.')[0]
+      str = "<div><a href='#{url}'>" 
+      
+      for ii in ['1', '2', '3', '4', '5'] 
+      
+        if (ii > val) 
+          str += "<img src='/images/addfav.png'>"
+        else 
+          str += "<img src='/images/rating.png'>"
+        end 
+        
+      end 
+      
+      str += "</a></div>"
+      str 
+    end
+
+
+
+
     def helper_rating_calc(valor)
       
       val = "#{valor}".split('.')[0]
