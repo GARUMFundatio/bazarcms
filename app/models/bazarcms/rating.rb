8cd99159df2a3b80689e7a498091fc63faca9c70
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-07-11 22:53:45 +0200

    Rating: calculo rating

--- app/models/bazarcms/rating.rb
+++ app/models/bazarcms/rating.rb
@@ -16,6 +16,7 @@ module Bazarcms
       total_rating_proveedor = 0
       total_val_proveedor = 0 
     
+      puts "Calculando la ficha de rating para bazar #{bazar} empresa #{empresa} "
       rats = Rating.where("(ori_bazar_id = ? and ori_empresa_id = ?) or (des_bazar_id = ? and des_empresa_id = ?)", bazar, empresa, bazar, empresa)
 
       hay = false 
@@ -27,14 +28,16 @@ module Bazarcms
         totrc = totrp = 0
         totc = totp = 0
         
-        if rat.ori_bazar_id = bazar and rat.ori_empresa_id = empresa           
+        if rat.ori_bazar_id == bazar && rat.ori_empresa_id == empresa           
           ori = true
         else 
           ori = false
         end 
-      
-        if ori
-
+  
+        puts "--->> bazar #{rat.ori_bazar_id} empresa #{rat.ori_empresa_id} "
+        
+        if ori == true
+          puts "------------> soy la empresa que inicia "
           if rat.role == 'C'
 
             if (rat.des_cliente_plazos > 0)  
@@ -66,7 +69,40 @@ module Bazarcms
 
           end
             
+        else 
+          puts "Estoy como empresa destino"
+          puts "------------> soy la empresa evaluada "
+          if rat.role == 'C'
 
+            if (rat.ori_proveedor_expectativas > 0)  
+              totrp += rat.ori_proveedor_expectativas
+              totp += 1
+            end
+
+            if (rat.ori_proveedor_plazos > 0)  
+              totrp += rat.ori_proveedor_plazos
+              totp += 1
+            end
+
+            if (rat.ori_proveedor_comunicacion > 0)  
+              totrp += rat.ori_proveedor_comunicacion
+              totp += 1
+            end 
+
+          else 
+
+            if (rat.ori_cliente_plazos > 0)  
+               totrc += rat.ori_cliente_plazos
+               totc += 1
+             end
+
+             if (rat.ori_cliente_comunicacion > 0)  
+               totrc += rat.ori_cliente_comunicacion
+               totc += 1
+             end
+
+          end         
+          
         end
 
 
@@ -83,15 +119,33 @@ module Bazarcms
 
       end 
 
-        #actualizamos los datos de la empresa 
-      
-        empresa = Empresa.find_by_id(empresa)
+      #actualizamos los datos de la empresa 
+    
+      empresa = Empresa.find_by_id(empresa)
 
+      if total_val_cliente > 0
         empresa.rating_cliente = total_rating_cliente/total_val_cliente
-        empresa.rating_total_cliente = total_val_cliente
-        empresa.rating_total_proveedor = total_val_proveedor
+      else 
+        empresa.rating_cliente = 0
+      end
 
-        empresa.save
+      if total_val_proveedor > 0
+        empresa.rating_proveedor = total_rating_proveedor/total_val_proveedor
+      else 
+        empresa.rating_proveedor = 0
+      end
+
+      if (total_val_cliente > 0 || total_val_proveedor > 0)
+        empresa.rating = (total_rating_cliente + total_rating_proveedor) / (total_val_cliente + total_val_proveedor)
+      else 
+        empresa.rating = 0        
+      end 
+      
+      empresa.rating_total_cliente = total_val_cliente
+      
+      empresa.rating_total_proveedor = total_val_proveedor
+
+      empresa.save
     
     end 
 
