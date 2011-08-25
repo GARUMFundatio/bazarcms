34908d794189471b256fc36e7d8b95f74a541d7b
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-08-26 00:43:28 +0200

    Resultados: busquedas añadiendo el logo y la empresa

--- app/controllers/bazarcms/ofertas_controller.rb
+++ app/controllers/bazarcms/ofertas_controller.rb
@@ -8,7 +8,24 @@ module Bazarcms
   before_filter :require_no_user, :only => [:show2, :busca]
   before_filter :require_user, :only => [:index, :edit, :create, :update, :dashboard, :enviabusqueda, :buscador, :estadobusqueda, :resultado, :sitios]
   
+#  uses_tiny_mce :options => {
+#                              :width => "100%",
+#                              :theme_advanced_resizing => true,
+#                              :theme_advanced_resize_horizontal => true,
+#                              :theme_advanced_toolbar_location => "top",
+#                            	:theme_advanced_toolbar_align => "left",
+#                            	:theme_advanced_statusbar_location => "bottom",
+##                             :theme_advanced_buttons1 => "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+##                             :theme_advanced_buttons2 => "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+##                             :theme_advanced_buttons3 => "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+##                             :theme_advanced_buttons4 => "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+##                             :theme_advanced_resizing => true,
+##
+#                              :plugins => %w{ table fullscreen }
+#                            }
+#    
   layout "bazar"
+  
   def index
 
     # @ofertas = Oferta.where("1 = 1").order("fecha desc").paginate(:per_page => 30, :page => params[:page])
@@ -89,6 +106,7 @@ module Bazarcms
   end
 
   def new
+    
     @oferta = Oferta.new
 
     logger.debug @oferta.inspect
