78c0e603df6d75644e5122e87335fc11a060b4d9
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2012-02-02 11:38:46 +0100

    ofertas: creando una función genérica de busqueda de ofertas2

--- app/models/bazarcms/oferta.rb
+++ app/models/bazarcms/oferta.rb
@@ -145,6 +145,91 @@ module Bazarcms
       @consulta.total_resultados = @consulta.total_resultados + conta2;
       @consulta.save
 
+      # looking for another bazares
+
+      conta = conta2 = 0 
+      
+      hydra = Typhoeus::Hydra.new
+
+      logger.debug "lanzo las peticiones "+DateTime.now.to_s
+
+      for cluster in @clusters
+
+        if micluster != cluster.id 
+
+          # uri = "#{cluster.url}/bazarcms/buscaofertas?q="+CGI.escape(q)+"&qtipo="+CGI.escape(tipo)+"&bid=#{@consulta.id}&cid=#{micluster}"
+          # /bazarcms/buscaofertas?q=bazar&qe=0+10&qv=0+10&qc=0+10&qr=0+10&pofertan=&pdemandan=&ppaises=&qtipo=D&bid=1&cid=8
+          
+          uri = "#{cluster.url}/bazarcms/buscaofertas?q="+CGI.escape(q)+"&qe="+CGI.escape("0")+"&qv="+CGI.escape("0")+"&qc="+CGI.escape("0 10")+"&qr="+CGI.escape("0 10")+"&pofertan="+CGI.escape("")+"&pdemandan="+CGI.escape("")+"&ppaises="+CGI.escape("")+"&qtipo="+CGI.escape(tipo)+"&bid=#{@consulta.id}&cid=#{micluster}"
+          logger.debug "Enviando Petición a ------------> #{uri}"
+
+          r = Typhoeus::Request.new(uri, :timeout => 5000)
+          r.on_complete do |response|
+            logger.debug "-------------> "+response.inspect
+            case response.curl_return_code
+            when 0
+              conta += 1
+              conta2 = 0
+
+              begin
+                ofertas = JSON.parse(response.body)
+
+                logger.debug "#{ofertas.inspect} <-----------"
+                cluster_id = 0 
+                ofertas.each{ |key|
+                  logger.debug("#{key.inspect}")
+                  if !key['cluster_id'].nil?
+                    logger.debug "viene un cluster id "+key.inspect
+                    cluster_id = key['cluster_id']
+                  end
+                  if !key['oferta'].nil?
+                    logger.debug("#{key['oferta'].inspect} <------ datos")
+
+                    resu = Bazarcms::Ofertasresultado.new()    
+                    resu.ofertasconsulta_id = @consulta.id
+                    resu.cluster_id = cluster_id
+                    resu.oferta_id = key['oferta']['id']
+                    resu.tipo =  key['oferta']['tipo']
+                    resu.empresa_id = key['oferta']['empresa_id'] 
+                    resu.nombre_empresa = "poner nombre bien"
+                    resu.enlace = "Poner el enlace bien 2" # key['oferta']['url']
+                    resu.orden = key['oferta']['fecha'] # .strftime('%Y%m%d%H%M')
+                    resu.info = key['oferta']['titulo']
+                    resu.save
+                    conta2 += 1
+                  end
+                  }
+
+                @consulta.total_respuestas = @consulta.total_respuestas + 1;
+                @consulta.total_resultados = @consulta.total_resultados + conta2;
+                @consulta.save
+              rescue 
+                logger.debug "El JSON: venía mal formado #{response.body}"
+
+              end 
+            else
+              logger.debug "ERROR en la petición ---------->"+response.inspect
+            end
+
+          end
+
+          hydra.queue r        
+
+        end
+
+      end 
+
+      hydra.run
+
+      logger.debug "servidas "+DateTime.now.to_s
+
+      @consulta.total_consultas = conta;
+      @consulta.fecha_fin = DateTime::now
+
+      @consulta.save
+
+
+
 
       # we return the results of the search 
       
