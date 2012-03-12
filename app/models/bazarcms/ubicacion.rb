b2f88c06cc3e0fad7a901e24c5c78198b060b163
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2012-03-12 13:03:56 +0100

    ubicaciones: calculando distancias entre ciudades

--- app/models/bazarcms/ubicacion.rb
+++ app/models/bazarcms/ubicacion.rb
@@ -7,7 +7,15 @@ module Bazarcms
     
     belongs_to :empresa
     belongs_to :ciudad
-        
+  
+  def self.distancia(desde, hasta)
+  
+    ori = Bazarcms::Ciudad.where("geocode = ?", desde)
+    des = Bazarcms::Ciudad.where("geocode = ?", hasta)
+    puts "Calculando la distancia entre #{ori.descripcion}:#{ori.geocode} y #{des.descripcion}:#{des.geocode}"
+    puts "#{ori.descripcion} = #{ori.longitud} #{ori.latitud}"
+    puts "#{des.descripcion} = #{des.longitud} #{des.latitud}"
+
   end
 
 end
